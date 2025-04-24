d62c5943bcdbaa38f5deb235ca028be31fe429f9
LuisElessar <SteveLuis4555@gmail.com>
2025-04-24 09:12:27 -0500

    Clases y git ignore
    pensé que ya había subido el coso creado xd

---
+++ ExamenTanakaProm1Progra2/ExamenTanakaProm1Progra2/IInterfaz.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenTanakaProm1Progra2
+{
+    internal interface IInterfaz
+    {
+    }
+}
